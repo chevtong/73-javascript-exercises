fa90de6ce7fb646d3c218a8c12ec4f2921773028
chevtong <chevtongwingyan@gmail.com>
2020-11-09 12:41:27 +0100

    Add exericse 1

--- 01-base/01-var-alert/script.js
+++ 01-base/01-var-alert/script.js
@@ -9,6 +9,10 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // your code here
-})();
+// (function() {
+//     // your code here
+// })();
+
+var x="Hello World!";
+
+alert(x);
