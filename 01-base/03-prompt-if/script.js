febd5f2ab0b4cb031a4290724bf267f8628b5906
chevtong <chevtongwingyan@gmail.com>
2020-11-09 16:48:11 +0100

    Add exercise 3

--- 01-base/03-prompt-if/script.js
+++ 01-base/03-prompt-if/script.js
@@ -9,8 +9,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
+// (function() {
+
+//   var cake =  prompt("Do you want some cake?");
+//   if (cake.toLowerCase() == "yes") {
+//     alert("Congrats!")
+//   } else {
+//       alert("Your loss, more for me then!")
+//     }
+
+// })();
+
+
 (function() {
 
   // your code here
+  var question = prompt("Do you want some cake?", "yes or no");
+
+  if (question == "yes") {
+    alert("congratulation!")
+  }
+  else {
+    alert("More cake for me then :p !")
+  }
 
 })();
+
