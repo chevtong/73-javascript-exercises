b58b47d3e9681de32f17b520f47fccc9dadec29f
chevtong <chevtongwingyan@gmail.com>
2020-11-10 09:51:31 +0100

    Add exercise04

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -9,22 +9,77 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
+(function () {
 
-    document.getElementById("addition").addEventListener("click", function() {
+
+    
+       // to get the value of an input: document.getElementById("element-id").value
+
+    document.getElementById("addition").addEventListener("click", function () {
         // perform an addition
+
+        
+    let x, y ,total;
+    x = parseFloat(document.getElementById("op-one").value);
+    y = parseFloat(document.getElementById("op-two").value);   
+
+    total = x + y;
+
+    alert(total);
+
+    console.log("x : "+ x);
+    console.log("y : "+ y);
+    console.log("result ="+total);
+             
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+
+    let x, y ,total;
+    x = parseFloat(document.getElementById("op-one").value);
+    y = parseFloat(document.getElementById("op-two").value);    
+    total = x - y;
+
+    alert(total);
+
+    console.log("x : "+ x);
+    console.log("y : "+ y);
+    console.log("result ="+total);
+    
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+
+    let x, y ,total;
+    x = parseFloat(document.getElementById("op-one").value);
+    y = parseFloat(document.getElementById("op-two").value);    
+    total = x * y;
+
+    alert(total);
+
+    console.log("x : "+ x);
+    console.log("y : "+ y);
+    console.log("result ="+total);
+            
+
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+
+        let x, y ,total;
+    x = parseFloat(document.getElementById("op-one").value);
+    y = parseFloat(document.getElementById("op-two").value);    
+    total = x / y;
+
+    alert(total);
+
+    console.log("x : "+ x);
+    console.log("y : "+ y);
+    console.log("result ="+total);
+        
+
     });
 })();
