ea12c549bf7389a19e7b0a937cec74a10ea45ef6
chevtong <chevtongwingyan@gmail.com>
2020-11-12 16:50:57 +0100

    Add ex4/dates

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -15,4 +15,52 @@
 
     // your code here
 
+    let d = new Date();
+
+    // weekday start 
+    let weekday = new Array(7);
+    weekday [0] = "Sunday"; 
+    weekday [1] = "Monday"; 
+    weekday [2] = "Tuesday"; 
+    weekday [3] = "Wednesday"; 
+    weekday [4] = "Thursday"; 
+    weekday [5] = "Friday"; 
+    weekday [6] = "Saturaday"; 
+
+    let weekdayInEnglish = weekday[d.getDay()];
+
+    // date start
+    let date = d.getUTCDate();
+
+    // month start
+    let month = new Array (12);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    let monthInEnglish = month[d.getMonth()];
+
+    // year start
+    let year = d.getFullYear();
+
+    // hour start
+    let hour = d.getHours();
+
+    // minutes start
+    let min = d.getMinutes();
+
+
+
+
+    document.getElementById("target").innerHTML = (weekdayInEnglish + " " + date + " " + monthInEnglish + " " + year + ", " + hour + "h" + min);
+
 })();
